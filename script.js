3804dda4a4c857c66242d9fd619647abbb1396e0
John <jarrington08@gmail.com>
2023-07-23 14:55:29 -0500

    Added function to validate forms contain data

--- script.js
+++ script.js
@@ -12,5 +12,17 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
+
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+        if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+            alert("Please fill out all fields.");
+            preventDefault();
+        }
+   });
    
 });
