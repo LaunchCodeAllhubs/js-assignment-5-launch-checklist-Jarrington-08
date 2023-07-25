1bd271a92b4b3c6e7487c29737ced20a2733c76e
john <jarrington08@gmail.com>
2023-07-24 23:38:46 -0500

    completed the myFetch() and formSubmission() functions)

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
-   }).then(function () {
+    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    let pilot = document.querySelector("input[name=pilotName]");
+    let copilot = document.querySelector("input[name=copilotName");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoLevel = document.querySelector("input[name=cargoMass]");
+    let list = document.getElementById("faultyItems");
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+    formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+    event.preventDefault();
+    });
    })
 
-   let form = document.querySelector("form");
-   form.addEventListener("submit", function(event) {
-        let pilotName = document.querySelector("input[name=pilotName]");
-        let copilotName = document.querySelector("input[name=copilotName");
-        let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        let cargoMass = document.querySelector("input[name=cargoMass]");
-        if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-            alert("Please fill out all fields.");
-            event.preventDefault();
-        }
-   });
+   
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,23 +17,91 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (testInput.length === 0) {
+    return "Empty";
+   } else if (isNaN(testInput)) {
+    return "Not a Number";
+   } return "Is a Number";
+}
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+        //initialize variables to check if pilot and copilot are ready
+        let pilotValid = false;
+        let copilotValid = false;
+        let fuelLevelValid = false;
+        let cargoLevelValid = false;
+
+        // Check if pilot form data is valid
+        if (validateInput(pilot) === "Empty") {
+            alert("All fields are required!");
+        } else if (validateInput(pilot) === "Is a Number") {
+            alert("Make sure to enter valid information for each field!");
+        } else {
+            pilotValid = true;
+            document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
+        }
+
+        // //Check if co-pilot form data is valid
+        if (validateInput(copilot) === "Empty") {
+            alert("All fields are required!");
+        } else if (validateInput(copilot) === "Is a Number") {
+            alert("Make sure to enter valid information for each field!");
+        } else {
+            copilotValid = true;
+            document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
+        }
+
+        // //check if fuel level form data is valid
+        if (validateInput(fuelLevel) === "Empty") {
+            alert("All fields are required!");
+        } else if (validateInput(fuelLevel) === "Not a Number") {
+            alert("Make sure to enter valid information for each field!");
+        } else {
+            fuelLevelValid = true;
+        }
+
+        // //check if cargo mass form data is valid
+        if (validateInput(cargoLevel) === "Empty") {
+            alert("All fields are required!");
+            // prevenetDefault();
+        } else if (validateInput(cargoLevel) === "Not a Number") {
+            alert("Make sure to enter valid information for each field!");
+        } else {
+            cargoLevelValid = true;
+        }
+        
+        //check if fuel level and cargo level meet requirements
+        if (pilotValid && copilotValid && fuelLevelValid && cargoLevelValid) {
+            if (Number(fuelLevel) >= 10000 && Number(cargoLevel) < 10000) {
+                document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch";
+                document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch";
+                document.getElementById("cargoStatus").innerHTML = "Cargo mass low enough for launch";
+                document.getElementById("launchStatus").style.color = "#419F6A";
+            } 
+            if (Number(fuelLevel) < 10000) {
+                document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
+                document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+                document.getElementById("launchStatus").style.color = "red";
+            } 
+            if (Number(cargoLevel) > 10000) {
+                document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+                document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+                document.getElementById("launchStatus").style.color = "#C7254E";
+            }
+            list.style.visibility = "visible";
+        }
+            
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
-
+    planetsReturned = (await fetch("https://handlers.education.launchcode.org/static/planets.json")).json();
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
